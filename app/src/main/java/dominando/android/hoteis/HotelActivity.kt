5da7a5ebc42960d873b8f3f19004b9272c2fab08
João Pedro <jpedrodaibello1999@outlook.com>
2020-07-02 11:57:05 -0300

    Implementando AlertDialog e DialogFragment

--- app/src/main/java/dominando/android/hoteis/HotelActivity.kt
+++ app/src/main/java/dominando/android/hoteis/HotelActivity.kt
@@ -52,6 +52,10 @@ class HotelActivity : AppCompatActivity(), HotelListFragment.OnHotelClickListene
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.action_info ->
+                AboutDialogFragment().show(supportFragmentManager, "sobre")
+        }
         return super.onOptionsItemSelected(item)
     }
 
